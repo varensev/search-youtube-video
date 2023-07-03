823e54b5d16c5f4b2a1153044f657f36d848889e
meVarensev <vitalik.unit@gmail.com>
2023-07-03 23:27:59 +0500

    feat: header link login

--- src/components/header/ui/header.tsx
+++ src/components/header/ui/header.tsx
@@ -1,18 +1,20 @@
 import {Link} from "react-router-dom";
 import styles from './header.module.scss';
 import logoImage from '../../../shared/assets/icons/sibdev-logo.svg';
+import {classNames} from "../../../shared/lib/class-names";
+import {APP_ROUTER} from "../../../providers/router/config/app-routes.constants";
 
 const Header = () => {
     return (
         <header className={styles.header}>
-            <div className={styles.container}>
+            <div className={classNames(styles.container, {}, ["container"])}>
                 <img src={logoImage} alt="Логотип" className={styles.logo}/>
                 <nav className={styles.links}>
                     <div>
                         <Link to="/">Поиск</Link>
                         <Link to="/">Избранное</Link>
                     </div>
-                    <Link to="/">Войти</Link>
+                    <Link to={APP_ROUTER.LOGIN}>Выйти</Link>
                 </nav>
             </div>
         </header>
