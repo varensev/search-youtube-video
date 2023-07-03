427cdd85507a87b0793bd3641b58d0bc1c6041ea
meVarensev <vitalik.unit@gmail.com>
2023-07-03 15:02:35 +0500

    refactor: loader scss

--- src/widget/loader/loader.tsx
+++ src/widget/loader/loader.tsx
@@ -5,12 +5,12 @@ import {classNames} from "../../shared/lib/class-names";
 function Loader() {
     return (
         <div className={styles.loader}>
-        <div className={classNames(styles.item, {},  [styles[styles.top]])} />
-    <div className={classNames(styles.item, {},  [styles[styles.left]])} />
-    <div className={classNames(styles.item, {},  [styles[styles.right]])} />
-    <div className={classNames(styles.item, {},  [styles[styles.bottom]])} />
-    </div>
-);
+            <div className={classNames(styles.item, {}, [styles[styles.top]])}/>
+            <div className={classNames(styles.item, {}, [styles[styles.left]])}/>
+            <div className={classNames(styles.item, {}, [styles[styles.right]])}/>
+            <div className={classNames(styles.item, {}, [styles[styles.bottom]])}/>
+        </div>
+    );
 }
 
 export default Loader;
