da85e00b338a8bc53f6c92879f4f97fe918b0d02
meVarensev <vitalik.unit@gmail.com>
2023-07-08 20:34:11 +0500

    feat: useGetMoviesByNameQuery in input-search.tsx

--- src/components/search/ui/search.tsx
+++ src/components/search/ui/search.tsx
@@ -1,8 +1,17 @@
 import {InputSearch} from "../../../shared/ui/components/input-search";
+import {Content} from "../../content";
+import {useGetMoviesByNameQuery} from "../../../store/api-query";
+import {useState} from "react";
 
 const Search = () => {
+    const [searchItem, setSearchItem] = useState("");
+    const {data, error, isLoading} = useGetMoviesByNameQuery(searchItem)
+
     return (
-        <InputSearch/>
+        <>
+            <InputSearch setSearchItem={setSearchItem} searchItem={searchItem}/>
+            {data && <Content youTubeSearchList={data.items} searchItem={searchItem}/>}
+        </>
     );
 };
 
