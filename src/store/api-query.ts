04c98745132cc936e0d992f25e496f90e6d83eb3
meVarensev <vitalik.unit@gmail.com>
2023-09-06 20:01:17 +0300

    feat: api key

--- src/store/api-query.ts
+++ src/store/api-query.ts
@@ -1,9 +1,9 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 import {Root} from "./youtube-types";
-import {apiKey} from "./api_key";
+
 
 const apiUrl = 'https://www.googleapis.com/youtube/v3/search';
-
+const apiKey = process.env.REACT_APP_API_KEY;
 export const youtubeApi = createApi({
 
     reducerPath: 'youtubeApi',
